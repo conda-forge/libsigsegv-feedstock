28fa7647a3bd707c7428bbe40d9beb1bab25770b
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 17:47:24 -0800

    update to 2.14

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
-./configure --prefix=$PREFIX --enable-shared --disable-static &&
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+./configure --prefix=$PREFIX \
+    --enable-shared \
+    --disable-static
 make
 make check
 make install
