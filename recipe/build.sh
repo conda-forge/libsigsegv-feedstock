2ebe68bb4b6902b638dfe72d9d3d93237d7e60ac
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 21:16:34 -0800

    fix testing

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,8 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 # Skip failing tests on linux-aarch64 and linux-ppc64le
 if [[ ${target_platform} == "linux-aarch64" || ${target_platform} == "linux-ppc64le" ]]; then
-    sed -i '/test-catch-segv1 /d' tests/Makefile.am
-    sed -i '/test-catch-segv2 /d' tests/Makefile.am
+    sed -i '/test-catch-stackoverflow1 /d' tests/Makefile.am
+    sed -i '/test-catch-stackoverflow2 /d' tests/Makefile.am
 fi
 
 export CFLAGS="${CFLAGS} -Wno-int-conversion"
