e38e11b43ab9046ff254ee9c45b7663731ad6f1d
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 22:04:17 -0800

    fix testing

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 # Skip failing tests on linux-aarch64 and linux-ppc64le
 if [[ ${target_platform} == "linux-aarch64" || ${target_platform} == "linux-ppc64le" ]]; then
+    sed -i 's/test-segv-dispatcher1 \\/test-segv-dispatcher1/' tests/Makefile.am
     sed -i '/test-catch-stackoverflow1/d' tests/Makefile.am
     sed -i '/test-catch-stackoverflow2/d' tests/Makefile.am
 fi
